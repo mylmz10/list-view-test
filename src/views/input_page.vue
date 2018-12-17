41f1ce514b1edea4a48623b56d72fea882d47d10
DESKTOP-2B7SBKL\Muko <anavatan40@gmail.com>
2018-12-17 20:26:14 +0300

    update

---
+++ src/views/input_page.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+  	<h3>Input v-model support not working if exist vue.config.js</h3>
+  	<div>
+    	<input type="text" v-model="username" />
+  	</div>
+  	<div>
+  		{{ username }}
+  	</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "input_page",
+  data() {
+    return {
+      username: "mylmz"
+    };
+  }
+};
+</script>
---
+++ src/views/vtemplate.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <ListView for="(item, index) in items">
+        <v-template if="$even">
+          <div style="background: #333; padding: 10px; margin-bottom: 10px;">
+            <div>{{ item }}</div>
+          </div>
+        </v-template>
+
+        <v-template>
+          <div style="background: #c0ffee; padding: 10px; margin-bottom: 10px;">
+            <div>{{ item }}</div>
+          </div>
+        </v-template>
+      </ListView>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      items: ["foo", "bar", "baz", "fizz", "fuzz"]
+    };
+  }
+};
+</script>
